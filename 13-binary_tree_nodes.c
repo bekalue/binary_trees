19d3db4316f0cb4c9f6aa4d4dc6980dcbf73f4fc
Bekalu Geta <bekalu2054@gmail.com>
2023-02-28 12:56:19 +0300

    ⚓fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		if (!tree->left && !tree->right && tree->parent)
+		if (!tree->left && !tree->right)
 			return (0);
 		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 	}
